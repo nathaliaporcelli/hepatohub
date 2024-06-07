51fdd4f49ecd95a2b0bdc8e810dcec92240922ab
rafael <calixxtorafell@gmail.com>
2024-06-07 07:15:12 -0300

    feat: adicionar criptografia de senhas para registro de usuários

--- src/main/java/com/example/hepatohub/controller/UserController.java
+++ src/main/java/com/example/hepatohub/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.example.hepatohub.controller;
 
-import com.example.hepatohub.model.User;
-import com.example.hepatohub.service.UserService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.hepatohub.dto.UserDTO;
+import com.example.hepatohub.model.User;
+import com.example.hepatohub.service.UserService;
 
 @RestController
 @RequestMapping("/user")
@@ -17,14 +23,13 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public List<com.example.hepatohub.model.User> list() {
+    public List<User> list() {
         return userService.listAllUsers();
     }
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody com.example.hepatohub.model.User user) {
-        User savedUser = userService.createUser(user);
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
+        User savedUser = userService.createUser(userDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
-
 }
--- src/main/java/com/example/hepatohub/service/UserService.java
+++ src/main/java/com/example/hepatohub/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.hepatohub.service;
 
-import com.example.hepatohub.model.User;
-import com.example.hepatohub.repository.UserRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.example.hepatohub.dto.UserDTO;
+import com.example.hepatohub.model.User;
+import com.example.hepatohub.repository.UserRepository;
 
 @Service
 public class UserService {
@@ -13,11 +16,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User createUser(User user) {
-        return userRepository.save(user);
-    }
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     public List<User> listAllUsers() {
         return userRepository.findAll();
+    }
+
+    public User createUser(UserDTO userDTO) {
+        User user = new User();
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        return userRepository.save(user);
     }
 }
